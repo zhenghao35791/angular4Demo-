f2766f8150565204adf6251d805ed9820d984dc9
zhenghao <zhenghao35791@gmail.com>
2017-08-24 15:27:17 +0800

    tttttttttttttttttttttttttttttt

---
+++ angular4demo/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { AppComponent } from './app.component';
+
+@NgModule({
+    declarations: [ // 声明组件
+        AppComponent
+    ],
+    // @NgModule元数据的imports数组中，它是当前应用正在使用的外部模块列表。
+    imports: [
+        BrowserModule,
+        FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { } // 根模块会告诉Angular如何组装该应用
