d37a22c71fbe231b76a2e74b42e59d0aed2e18ed
zhenghao <zhenghao35791@gmail.com>
2017-08-24 17:05:00 +0800

    把原来的AppComponent重构成了两个组件具有一些显著优点，无论是现在还是未来：
    通过缩减AppComponent的职责，我们简化了它。
    我们将来可以把HeroDetailComponent改进为功能更丰富的英雄编辑器，而不用动AppComponent。
    同样，我们也可以改进AppComponent而不用动英雄详情视图。
    我们可以在未来的其它父组件的模板中复用HeroDetailComponent。

--- angular4demo/src/app/hero-detail.component.ts
+++ angular4demo/src/app/hero-detail.component.ts
@@ -7,11 +7,12 @@ import { Hero } from './hero';
 })
 
 /*
-hero属性是HeroDetailComponent类中唯一的东西。
-它所做的一切就是通过它的输入属性hero接收一个英雄对象，然后把这个属性绑定到自己的模板中。
-
+heroDetail属性是HeroDetailComponent类中唯一的东西。
+它所做的一切就是通过它的输入属性heroDetail接收一个英雄对象，
+然后把这个属性绑定到自己的模板中。
+在heroDetail属性前面加上@Input装饰器，来表明它是一个输入属性。
 */
 
 export class HeroDetailComponent {
-    @Input() hero: Hero;
+    @Input() heroDetail: Hero;
 }
